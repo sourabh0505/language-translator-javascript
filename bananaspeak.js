547d04a803a422f311c2eccb250989c72b848c9b
sourabh0505 <105512400+sourabh0505@users.noreply.github.com>
2022-07-01 15:46:44 +0530

    message translator

---
+++ bananaspeak.js
@@ -0,0 +1,28 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var Output = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error is occured!", error);
+    alert("the server is down!");
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var Translatedtxt = json.contents.translated
+            output.innerText = Translatedtxt;
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
