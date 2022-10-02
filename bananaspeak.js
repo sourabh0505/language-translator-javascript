2867e057df927744ce930f0f682b0d36f4e9417b
sourabh0505 <105512400+sourabh0505@users.noreply.github.com>
2022-10-02 13:24:09 +0530

    changed the api link

--- bananaspeak.js
+++ bananaspeak.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var Output = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input;
